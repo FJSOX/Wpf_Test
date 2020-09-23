62239edbdd66577e2ddbb7285d9ff873ba9fca59
劲松 冯 <503868944@qq.com>
2020-09-23 15:08:30 +0800

    添加项目文件。

---
+++ MainViewMode1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Wpf_Test
+{
+    public class MainViewMode1 : INotifyPropertyChanged
+    {
+        public MainViewMode1()
+        {
+            Name = "hhh";
+            Task.Run(async() =>
+            {
+                await Task.Delay(1000);//时间间隔
+                Name = "qq";//操作内容
+            });
+            
+        }
+
+        private string name;
+        public string Name { get { return name; }
+            set { name = value;
+                OnPropertyChanged("Name");
+            } }
+        
+        public event PropertyChangedEventHandler PropertyChanged;
+    
+        protected void OnPropertyChanged(string properName)
+        {
+            if (PropertyChanged!=null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(properName));
+            }
+        }
+    }
+}
